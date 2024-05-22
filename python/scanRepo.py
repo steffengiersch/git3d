0bd1a8cb6154309093996d255b8d8380355390fc
Oliver Widder <oliver.widder@iteratec.com>
2024-05-22 15:17:11 +0200

    recursive_walk_repo (5)

--- python/scanRepo.py
+++ python/scanRepo.py
@@ -18,37 +18,51 @@ def extract_author_list(commits_touching_path) -> list:
     for commit in commits_touching_path:
         points = 1 / (((datetime.now() - datetime.fromtimestamp(commit.committed_date)).days // 7) + 1)
         authors[commit.author.name] = authors.get(commit.author.name, 0) + points
-    authors_list = reduce(lambda al, a: [*al, {'author': a, 'knowledge': authors[a]}], authors.keys(), [])
-    return authors_list
+    author_list = reduce(lambda al, a: [*al, {'author': a, 'knowledge': authors[a]}], authors.keys(), [])
+    return author_list
+
+
+def merge_author_lists(target_list: list, source_list: list) -> list:
+    target_dict = reduce(lambda td, a: {**td, a['author']: a['knowledge']}, target_list, {})
+    for source_entry in source_list:
+        if source_entry['author'] in target_dict:
+            target_dict[source_entry['author']] += source_entry['knowledge']
+        else:
+            target_dict[source_entry['author']] = source_entry['knowledge']
+    return reduce(lambda tl, a: [*tl, {'author': a, 'knowledge': target_dict[a]}], target_dict.keys(), [])
 
 
 def recursive_walk_repo(repo, relative_path, current):
     git = repo.git
     repo_path = git.working_dir
     abs_path = os.path.join(repo_path, relative_path)
-    if os.path.isfile(abs_path) and is_file_in_git(git, relative_path):
-        commits_touching_path = list(repo.iter_commits(paths=relative_path))
-        number_of_changes = len(commits_touching_path)
-        author_list = extract_author_list(commits_touching_path)
-        criticality = reduce(lambda c, a: c + a['knowledge'], author_list, 0)
-        size_in_bytes = os.path.getsize(abs_path)
-        file_descriptor = {
-            'type': 'file',
-            'name': os.path.basename(relative_path),
-            'extension': os.path.splitext(relative_path)[1],
-            'created': time.ctime(os.path.getctime(abs_path)),
-            'last_modified': time.ctime(os.path.getmtime(abs_path)),
-            'changes': number_of_changes,
-            'sizeInBytes': size_in_bytes,
-            'contributors': author_list,
-            'criticality': criticality,
-        }
-        current['children'].append(file_descriptor)
-        return (number_of_changes, criticality, size_in_bytes)
+    if os.path.isfile(abs_path):
+        if is_file_in_git(git, relative_path):
+            commits_touching_path = list(repo.iter_commits(paths=relative_path))
+            number_of_changes = len(commits_touching_path)
+            author_list = extract_author_list(commits_touching_path)
+            criticality = reduce(lambda c, a: c + a['knowledge'], author_list, 0)
+            size_in_bytes = os.path.getsize(abs_path)
+            file_descriptor = {
+                'type': 'file',
+                'name': os.path.basename(relative_path),
+                'extension': os.path.splitext(relative_path)[1],
+                'created': time.ctime(os.path.getctime(abs_path)),
+                'last_modified': time.ctime(os.path.getmtime(abs_path)),
+                'changes': number_of_changes,
+                'sizeInBytes': size_in_bytes,
+                'contributors': author_list,
+                'criticality': criticality,
+            }
+            current['children'].append(file_descriptor)
+            return (number_of_changes, criticality, size_in_bytes, author_list)
+        else:
+            return (0, 0, 0, [])
     else:
         sum_number_of_changes = 0
         sum_criticality = 0
         sum_size_in_bytes = 0
+        sum_author_list = []
         folder_descriptor = {
             'type': 'folder',
             'name': os.path.basename(relative_path),
@@ -60,15 +74,17 @@ def recursive_walk_repo(repo, relative_path, current):
             abs_entry_path = os.path.join(str(abs_path), entry)
             if os.path.isfile(abs_entry_path) or not entry.startswith('.'):
                 rel_entry_path = os.path.join(relative_path, entry)
-                (criticality, number_of_changes, size_in_bytes) = recursive_walk_repo(repo=repo, relative_path=rel_entry_path, current=folder_descriptor)
+                (criticality, number_of_changes, size_in_bytes, author_list) = recursive_walk_repo(repo=repo, relative_path=rel_entry_path, current=folder_descriptor)
                 sum_number_of_changes += number_of_changes
                 sum_criticality += criticality
                 sum_size_in_bytes += size_in_bytes
+                sum_author_list = merge_author_lists(sum_author_list, author_list)
         folder_descriptor['criticality'] = sum_criticality
         folder_descriptor['sizeInBytes'] = sum_size_in_bytes
         folder_descriptor['number_of_changes'] = sum_number_of_changes
+        folder_descriptor['contributors'] = sum_author_list
         current['children'].append(folder_descriptor)
-        return (sum_number_of_changes, sum_criticality, sum_size_in_bytes)
+        return (sum_number_of_changes, sum_criticality, sum_size_in_bytes, sum_author_list)
 
 
 def start(repo_path, out_file_path):
