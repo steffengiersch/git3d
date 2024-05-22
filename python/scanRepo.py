36a892358a618652943383da1b16eb03dbb600ba
Oliver Widder <oliver.widder@iteratec.com>
2024-05-22 17:27:59 +0200

    use objects for contributors

--- python/scanRepo.py
+++ python/scanRepo.py
@@ -20,7 +20,7 @@ def extract_author_list(commits_touching_path: list) -> list:
     authors = {}
     for commit in commits_touching_path:
         if commit.author.name not in global_author_list:
-            global_author_list.append(commit.author.name)
+            global_author_list.append({'name': commit.author.name})
         if commit.author.name not in authors:
             points = 1 / (((datetime.now() - datetime.fromtimestamp(commit.committed_date)).days // 30) + 1)
             authors[commit.author.name] = points
