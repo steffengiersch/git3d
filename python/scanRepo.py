141a90614f065c85b67414b346ef732bd0de4249
Oliver Widder <oliver.widder@iteratec.com>
2024-05-22 11:44:32 +0200

    remove REPO_PATH

--- python/scanRepo.py
+++ python/scanRepo.py
@@ -1,8 +1,6 @@
 from git import Repo
 import os
 
-REPO_PATH = os.getenv('REPO_PATH')
-
 
 def is_file_in_git(git, file_path: str):
     """Check if file is tracked by git."""
@@ -13,16 +11,17 @@ def is_file_in_git(git, file_path: str):
         return False
 
 
-def walk_through_repo(git, folder_path: str):
-    for dirpath, dirnames, filenames in os.walk(folder_path):
+def walk_through_repo(git):
+    repo_path = git.working_dir
+    for dirpath, dirnames, filenames in os.walk(repo_path):
         for file_name in filenames:
             full_path = os.path.join(dirpath, file_name)
-            relative_path = os.path.relpath(str(full_path), folder_path)
+            relative_path = os.path.relpath(str(full_path), repo_path)
             if is_file_in_git(git, relative_path):
                 print(relative_path)
 
 
 if '__main__' == __name__:
-    repo = Repo(REPO_PATH)
+    repo = Repo(os.getenv('REPO_PATH'))
     git = repo.git
-    walk_through_repo(git, repo.working_dir)
+    walk_through_repo(git)
